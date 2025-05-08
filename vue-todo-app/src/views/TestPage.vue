5aabda7ee0b25a3435f022b6597acd82803e66b6
Ana Bacic <ana.bacic132@gmail.com>
2025-05-08 16:43:58 +0200

    added test page

---
+++ vue-todo-app/src/views/TestPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+      <div class="bg-blue-500 text-white p-8 rounded-xl shadow text-center w-full max-w-md">
+        <h1 class="text-2xl font-bold mb-6">Testna stranica</h1>
+        <Button class="text-white">Testni gumb</Button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { Button } from '@/components/ui/button'
+  </script>
+  
